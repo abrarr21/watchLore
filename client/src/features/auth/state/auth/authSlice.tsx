f5b82c574337b1ca699f9cf2dcddae7410a7567c
abrarr21 <abrarrr2110@gmail.com>
2026-06-23 14:16:36 +0530

    feat(auth): set up redux store and authSlice

---
+++ client/src/features/auth/state/auth/authSlice.tsx
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: null,
+    isLoading: false,
+  },
+  reducers: {
+    addUser: (state, action) => {
+      state.user = action.payload;
+      state.isLoading = false;
+    },
+    removeUser: (state) => {
+      state.user = null;
+      state.isLoading = false;
+    },
+  },
+});
+
+export const { addUser, removeUser } = authSlice.actions;
+
+export default authSlice.reducer;
